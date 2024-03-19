3feafcfca782578effd2801663145694a4f43601
Didier A <akda.industries@gmail.com>
2024-03-19 23:15:38 +0100

    Verif email OK

---
+++ Aura/Aura/Views/VueTest.swift
@@ -0,0 +1,18 @@
+//
+//  VueTest.swift
+//  Aura
+//
+//  Created by Kouassi Assoua on 17/03/2024.
+//
+
+import SwiftUI
+
+struct VueTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VueTest()
+}
